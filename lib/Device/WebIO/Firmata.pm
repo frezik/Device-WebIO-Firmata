17cdbba5f472a18e589544394cfdde5b3ff04e9f
tmurray <tmurray@wumpus-cave.net>
2014-08-28 18:09:31 -0500

    Add docs

--- lib/Device/WebIO/Firmata.pm
+++ lib/Device/WebIO/Firmata.pm
@@ -1,3 +1,26 @@
+# Copyright (c) 2014  Timm Murray
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions are met:
+# 
+#     * Redistributions of source code must retain the above copyright notice, 
+#       this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright 
+#       notice, this list of conditions and the following disclaimer in the 
+#       documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
 package Device::WebIO::Firmata;
 
 # ABSTRACT: Interface between Device::WebIO and Device::Firmata (Arduino)
@@ -134,3 +157,79 @@ sub adc_pin_count
 
 
 1;
+
+
+=head1 NAME
+
+  Device::WebIO::Firmata - Access Arduino pins via Firmata via Device::WebIO
+
+=head1 SYNOPSIS
+
+    use Device::Firmata;
+    use Device::WebIO;
+    use Device::WebIO::Firmata;
+    
+    my $webio = Device::WebIO->new;
+    my $firmata = Device::WebIO::Firmata->new({
+        port => '/dev/ttyACM0',
+    });
+    $webio->register( 'foo', $firmata );
+    
+    my $value = $webio->adc_input_int( 'foo', 0 );
+
+=head1 DESCRIPTION
+
+Firmata is a protocol for accessing the pins of an Arduino connected to your 
+system over USB.  Device::WebIO provides a consistent interface in Perl for 
+accessing these pins.  Device::WebIO::Firmata provides the glue to bring them 
+together.
+
+After registering this with the main Device::WebIO object, you shouldn't need 
+to access anything in the Firmata object.  All access should go through the 
+WebIO object.
+
+=head1 IMPLEMENTED ROLES
+
+=over 4
+
+=item * DigitalOutput
+
+=item * DigitalInput
+
+=item * PWM
+
+=item * ADC
+
+=back
+
+=head1 SEE ALSO
+
+C<Device::WebIO>
+
+=head1 LICENSE
+
+Copyright (c) 2014  Timm Murray
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are 
+permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of 
+      conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of
+      conditions and the following disclaimer in the documentation and/or other materials 
+      provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS 
+OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+
+=cut
